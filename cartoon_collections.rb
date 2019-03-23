b18d98ceb23bb3491004607312ee29926fbb2558
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 20:31:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.include?(cheese_types)
 end
