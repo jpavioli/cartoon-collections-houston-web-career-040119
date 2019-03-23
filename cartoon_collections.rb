f2bdb43200253b7fb4dfdb9df16b69dc7460d5d4
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 20:33:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |might_be_cheese|
-    might_be_cheese.any?
+    might_be_cheese.any? do |word|
+      
 end
